f73f5c2fde4b80b32b9ef1e4da76dca703189565
backycloud <backycloud@gmail.com>
2013-10-09 14:18:57 +0400

    Update subprocess.py

--- subprocess.py
+++ subprocess.py
@@ -1,10 +1,14 @@
 import subprocess
-command = "find / -name myfile.ext"
+command = "df -h"
 proc = subprocess.Popen(command,stdout=subprocess.PIPE,shell=True)
 (out, err) = proc.communicate()
 outwithoutreturn = out.rstrip('\n')
 print out 
-./myfile.ext
+
+
+
+
+#./myfile.ext
 
 
 print outwithoutreturn
